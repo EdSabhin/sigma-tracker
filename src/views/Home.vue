4af2c63abd874cfb2f4d5e87df2a2ff84b677a5d
EdSabhin <edsabhin@yahoo.com>
2024-01-08 17:38:02 -0500

    Minor Updates.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { ref, watch } from "vue";
 import { useFetch } from "../service/fetch.js";
-import Navbar from "../components/Navbar.vue";
-import Footer from "../components/Footer.vue";
 import Hero from "../components/Hero.vue";
 
 const currencies = ref([]);
